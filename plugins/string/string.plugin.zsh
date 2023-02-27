31a328edda429eb7361d2e2a768b05e771090b69
mattmc3 <mattmc3@gmail.com>
2023-02-27 08:38:37 -0500

    Update string plugin

--- plugins/string/string.plugin.zsh
+++ plugins/string/string.plugin.zsh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+
+##? string - manipulate strings
 function string {
   if [[ ! -t 0 ]] && [[ -p /dev/stdin ]]; then
     if (( $# )); then
@@ -15,64 +17,44 @@ function string {
   fi
 }
 
+##? string-escape - escape special characters
 function string-escape {
   (( $# )) || return 1
-  local s; for s in "$@"; echo ${s:q}
+  local s
+  for s in "$@"; do
+    echo ${s:q}
+  done
 }
 
-function string-unescape {
-  (( $# )) || return 1
-  local s; for s in "$@"; echo ${s:Q}
-}
-
-function string-length {
-  (( $# )) || return 1
-  local s; for s in "$@"; echo $#s
-}
-
-function string-lower {
-  (( $# )) || return 1
-  local s; for s in "$@"; echo ${s:l}
-}
-
-function string-upper {
-  (( $# )) || return 1
-  local s; for s in "$@"; echo ${s:u}
-}
-
+##? string-join - join strings with delimiter
 function string-join {
   (( $# )) || return 1
   local sep=$1; shift
   echo ${(pj.$sep.)@}
 }
 
+##? string-join0 - join strings with null character
 function string-join0 {
-  string-join $'\0' "$@"
-}
-
-function string-split {
   (( $# )) || return 1
-  local s sep=$1; shift
-  for s in "$@"; printf '%s\n' "${(@ps.$sep.)s}"
+  string-join $'\0' "$@" ""
 }
 
-function string-split0 {
-  string-split $'\0' "$@"
-}
-
-function string-trim {
+##? string-length - print string lengths
+function string-length {
   (( $# )) || return 1
-  for s in "$@"; echo $s | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
-}
-
-function string-sub {
-  local s; local -A opts
-  zparseopts -D -A opts -- s: l:
+  local s
   for s in "$@"; do
-    echo ${s:${opts[-s]:-0}:${opts[-l]:-$#s}}
+    echo $#s
   done
 }
 
+##? string-lower - convert strings to lowercase
+function string-lower {
+  (( $# )) || return 1
+  local s; for s in "$@"; echo ${s:l}
+}
+
+##? string-pad - pad strings to a fixed width
 function string-pad {
   local s d pad; local -A opts=(-c ' ' -w 0)
   zparseopts -D -K -A opts -- c: w: r
@@ -82,4 +64,63 @@ function string-pad {
     pad="$d:$opts[-w]::$opts[-c]:"
     eval "echo \"\${($pad)s}\""
   done
+}
+
+##? string-repeat - multiply a string
+function string-repeat {
+  (( $# )) || return 1
+  local s n; local -A opts
+  zparseopts -D -A opts -- n: m: N
+  n=${opts[-n]:-$opts[-m]}
+  for s in "$@"; do
+    if [[ -v opts[-m] ]]; then
+      s=$(printf "$s%.0s" {1..$n})
+      printf ${s:0:$opts[-m]}
+    else
+      printf "$s%.0s" {1..$n}
+    fi
+    [[ -v opts[-N] ]] || printf '\n'
+  done
+}
+
+##? string-unescape - expand escape sequences
+function string-unescape {
+  (( $# )) || return 1
+  local s
+  for s in "$@"; do
+    echo ${s:Q}
+  done
+}
+
+##? string-split - split strings by delimiter
+function string-split {
+  (( $# )) || return 1
+  local s sep=$1; shift
+  for s in "$@"; printf '%s\n' "${(@ps.$sep.)s}"
+}
+
+##? string-split0 - split strings by null character
+function string-split0 {
+  string-split $'\0' "$@"
+}
+
+##? string-sub - extract substrings
+function string-sub {
+  local s; local -A opts
+  zparseopts -D -A opts -- s: l:
+  for s in "$@"; do
+    echo ${s:${opts[-s]:-0}:${opts[-l]:-$#s}}
+  done
+}
+
+##? string-trim -  remove trailing whitespace
+function string-trim {
+  (( $# )) || return 1
+  for s in "$@"; echo $s | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
+}
+
+##? string-upper - convert strings to uppercase
+function string-upper {
+  (( $# )) || return 1
+  local s; for s in "$@"; echo ${s:u}
 }
